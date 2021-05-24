6594ec5f719f7612c56d0f9a6547939ce4326689
raekwonza <34821012+raekwonza@users.noreply.github.com>
2021-05-24 18:14:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,45 @@
 import random
 
 border = []
+new_border = []
 values = [2, 4]
 
-for row in range(4):
-    for col in range(4):
-        border.append(random.choice(values))
-    print()
+
+def create_emp_border(some_border):
+    for row in range(4):
+        for col in range(4):
+            some_border.append("-")
+    return some_border
 
 
-for i in range(4):
-    for j in range(4):
-        print(border[j], end=" ")
-    print()
+def create_random_border(some_border):
+    for row in range(4):
+        for col in range(4):
+            some_border.append(random.choice(values))
+        print()
+    return some_border
+
+
+def print_border(some_border):
+    for row in range(4):
+        for col in range(4):
+            print(some_border[col], end=" ")
+        print()
+
+
+def start_move(some_border):
+    first_point_x = random.randint(1, 2)
+    first_point_y = random.randint(1, 2)
+
+    second_point_x = random.randint(1, 2)
+    second_point_y = random.randint(1, 2)
+
+    some_border = some_border[:first_point_x][:first_point_y] + random.choice(values) + some_border[first_point_x][first_point_y]
+    some_border = some_border[:second_point_x][:second_point_y] + random.choice(values) + some_border[second_point_x][second_point_y]
+
+    return some_border
+
+
+create_emp_border(border)
+start_move(border)
+print_border(border)
